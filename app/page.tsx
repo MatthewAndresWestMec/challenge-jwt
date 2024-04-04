bfc8b9ab075bfeac5db8e5cd0fac369e59923992
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2024-04-03 23:34:15 -0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
-"use client"
+'use client';
 
-import {redirect} from "next/navigation";
-import { login, logout} from './lib'
+import { redirect } from 'next/navigation';
+import { login, logout } from './lib';
 
 export default function SignIn() {
-
-  return(
+  return (
     <section>
-      <form action={
-        async(formdata) =>{
+      <form
+        action={async (formdata) => {
           var success = await login(formdata);
-          if(success){redirect('/home');}else{
-            console.log('login failed')
+          if (success) {
+            redirect('/home');
+          } else {
+            console.log('login failed');
           }
-        }}
-        ><input type="email" name="email" id="email"/>
-        <input type="password" name="password" id="password"/>
-        <input type="submit" name="submit" id="submit"/>
-        </form>
+        }}>
+        <input type='email' name='email' id='email' />
+        <input type='password' name='password' id='password' />
+        <input type='submit' name='submit' id='submit' />
+      </form>
 
-        <form action={
-        async(formdata) =>{
+      <form
+        action={async (formdata) => {
           await logout();
           redirect('/');
-        }}><button type='submit'>Logout</button>
-    
-        </form>
+        }}>
+        <button type='submit'>Logout</button>
+      </form>
+      <h1>Try this email: mandre361@west-mec.org password 1234</h1>
     </section>
   );
 }
