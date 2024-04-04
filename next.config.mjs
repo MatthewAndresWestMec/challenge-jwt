28d396fe8585e671e4fde179a8e855285797ff0a
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2024-04-03 23:46:09 -0700

    changed env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        SECRET_KEY: "eggs and bacon",
-        SECRET_PASS: 1234,
-        SECRET_EMAIL: 'mandre361@west-mec.org'
-    }
+  env: {
+    SECRET_KEY: 'eggs and bacon',
+    SECRET_PASS: '1234',
+    SECRET_EMAIL: 'mandre361@west-mec.org',
+  },
 };
 
 export default nextConfig;
